b7f01a86002531d182cdb952ba63ea8a8fb026db
Aedan H.P. Nicholls <99621280+Aedan-Paltridge-Nicholls@users.noreply.github.com>
2023-06-15 15:03:45 +1200

    Adding teachers

--- 103,2.cpp
+++ 103,2.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-void startup()
+char* Fixer(string InpStr)
+{
+  std::string Str17 = InpStr;
+  char *Fixer = new char[Str17.length()+1];//Creates the char* fixer and makes it one longer than str7.
+  strcpy(Fixer,Str17.c_str());//Makes the char* fixer and makes have the same content as str7.
+  return Fixer;
+}
+void startup()//Checks if the files for the programm exist and if not creates them.
 {
  system("if not exist C:\\logins\\Teachers mkdir C:\\logins\\Teachers");
 }
+void AddATeacher()
+{ 
+  string Check = "if not exist ";
+  string path = "C:\\logins\\Teachers";//Path to folder;
+  string command = " (mkdir ";
+  string command2 = " else (echo Teacher already exists) ";
+  string name ="\\";
+  string input ="";
+  cout <<"Name of Teacher : ";
+  cin >>  input ;
+  name.append(input);
+  path.append(name);
+  command.append(path);
+  string finalcommand = Check + path + command + ')' + command2;
+  Fixer(finalcommand);
+  system(Fixer(finalcommand));//
+  system("TREE C:\\logins");
+}
+void AddAClass()
+{ 
 
+}
 
 int main()
 {
-    
-    startup();
-    cout << "Hello World!\n";
+   system("TREE C:\\logins");
+   startup();
+   AddATeacher();
+  cout << "Hello World!\n";
 }
