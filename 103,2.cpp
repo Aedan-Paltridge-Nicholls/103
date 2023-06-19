aa611d68760bdbbc80d2a8dfb4a18155d3b2b94e
Aedan Nicholls <Aedan.Nicholls@techtorium.ac.nz>
2023-06-19 15:14:59 +1200

    stuff

--- 103,2.cpp
+++ 103,2.cpp
@@ -28,6 +28,7 @@ string Formatter(string input) // Formatts strings to replace spaces with unders
 void startup() // Checks if the files for the programm exist and if not creates them.
 {
   system("if not exist C:\\logins\\Teachers mkdir C:\\logins\\Teachers");
+  system("tree /F  C:\\logins");
 }
 void makefile(string content, string directory, string filename) // Make a text file in  a choosen  directory.
 {
@@ -36,15 +37,6 @@ void makefile(string content, string directory, string filename) // Make a text 
   string command = " echo. ";
   string AA = " >> "; // Two arrows so that, it will add to a file, not overwrite it.
   string out = command + Qm + content + Qm + AA + directory + Bs + filename + ".txt";
-  system(Fixer(out));
-}
-void makefile2(string content, string directory, string filename) // Make a text file in  a choosen  directory.
-{
-  char Bs = 92; // A Back slash
-  char Qm = 34; // A Quotation mark.
-  string command = " echo. ";
-  string AA = " >> "; // Two arrows so that, it will add to a file, not overwrite it.
-  string out = command  + content  + AA + directory + Bs + filename + ".txt";
   system(Fixer(out));
 }
 void makefolder(string input, string directory, string warning) // Make folder in  a choosen  directory.
@@ -75,7 +67,6 @@ void AddATeacher() // WIP
 }
 void AddAClass() // WIP
 {
-  Formatter("Nathan Black");
   char Bs = 92;                            // A Back slash
   string TeacherName;                      // The teachers name
   string ClassName;                        // The new class's name
@@ -97,24 +88,40 @@ void AddAClass() // WIP
   string Fullpath = path + Bs + ClassName;
   makefile("Test ", Fullpath, ClassName); // makes the file
 }
-void StudentInfoFile(string str1,string str2,string str3,string str4,string str5,string str6)
+void StudentInfoFile(string str1, string str2, string str3, string str4, string str5, string str6, string str7)
 {
-  /* string StudentName   =   str1                 // the name of the student
+  /*
+  string StudentName      =   str1                 // the name of the student
   string StudentAge       =   str2                 // the age of the student
   string Studentparents   =   str3                 // the parents of the student
   string phonenumber;     =   str4                 // the student's parent's phone number.
   string Studentaddress;  =   str5                 // the address of the student
   string Yearlevel;       =   str6                 // the Year level of the student
-  formatt 
-  Name: "StudentName" Age: "StudentAge" Year level: "Yearlevel" Home Address: "Studentaddress"
-  Name of parents: "Studentparents"  parents phonenumber: "phonenumber" 
+  string Path             =   str7                 // the path to the file
+  formatt
+  Name: "StudentName"  Age: "StudentAge"   Year level: "Yearlevel"
+  Home Address: "Studentaddress"
+  Name of parents: "Studentparents"  parents phonenumber: "phonenumber"
   */
- char Bs = 92;                // A Back slash
-  char Ob = 40;                // A Open bracket.
-  char Cb = 41;                // A Closed bracket.
-  char Sp = 32;                // A space.
-  char Qm = 34;                // A Quotation mark.
+  string Path = str7 + Formatter(str1); // the path to the file
+  string Tb = "<TAB>";                  // Adds a tab
+  string SC = "(echo.";                 // Starts the command
+  string EC = ") >>";                   // Ends the command
+  string NL = "& echo. & echo";         // adds a new line to a file
+  string L1;                            // line one
+  string L2;                            // line two
+  string L3;                            // line three
+  char Bs = 92;                         // A Back slash
+  char Ob = 40;                         // A Open bracket.
+  char Cb = 41;                         // A Closed bracket.
+  char Sp = 32;                         // A space.
+  char Qm = 34;                         // A Quotation mark.
 
+  L1 = "Name:" + Sp + str1 + Tb + str1 + Sp + "'s Age:" + Sp + str2 + Tb + str1 + Sp + "'s Yearlevel" + Sp + str3;
+  L2 = "Home address of" + Sp + str1 + Sp + ":" + Sp + str4;
+  L3 = "Name of" + Sp + str1 + Sp + "'s Parents:" + Sp + str5 + Tb + str1 + Sp + "'s Parents Phonenumbers" + Sp + str6;
+  string command = SC + L1 + NL + L2 + NL + L3 + EC + Path;
+  system(Fixer(command));
 }
 void AddStudent()
 {
@@ -130,52 +137,73 @@ void AddStudent()
   string Yearlevel;                        // the Year level of the student
   string Warning = "Class already exists"; // Warns if the inputed class already exists;
   // Other info.↓
-  cout << "The student's teacher's name : ";        // The students teacher's name
+  cout << "The student's teacher's name : "<< endl; // The students teacher's name
   getline(cin, TeacherName);
   TeacherName = Formatter(TeacherName);
-  cout << "The student's class's  name : ";         // the name of the student's class
+  cout << "The student's class's  name : "; // the name of the student's class
   getline(cin, ClassName);
   ClassName = Formatter(ClassName);
   // The  Student's info. ↓
-  cout << "The student's  name : ";                 // the name of the student
+  cout << "The student's  name : "; // the name of the student
   getline(cin, StudentName);
-  StudentName = Formatter(StudentName);
-  cout << "The student's  age : ";                  // the age of the student
+  // StudentName = Formatter(StudentName);
+  cout << "The student's  age : "; // the age of the student
   getline(cin, StudentAge);
-  StudentAge = Formatter(StudentAge);
-  cout << "The student's  parents names : ";        // the parents of the student
+  // StudentAge = Formatter(StudentAge);
+  cout << "The student's  parents names : "; // the parents of the student
   getline(cin, Studentparents);
-  Studentparents = Formatter(Studentparents);
+  //  Studentparents = Formatter(Studentparents);
   cout << "The student's parent's phone number : "; // the student's parent's phone number.
   getline(cin, phonenumber);
-  phonenumber = Formatter(phonenumber);
-  cout << "The student's  address : ";              // the address of the student
+  // phonenumber = Formatter(phonenumber);
+  cout << "The student's  address : "; // the address of the student
   getline(cin, Studentaddress);
-  Studentaddress = Formatter(Studentaddress);
-  cout << "The student's  Year level : ";           // the Year level of the student
+  // Studentaddress = Formatter(Studentaddress);
+  cout << "The student's  Year level : "; // the Year level of the student
   getline(cin, Yearlevel);
-  Yearlevel = Formatter(Yearlevel);
+  // Yearlevel = Formatter(Yearlevel);
   // ↑Inputs ↓File and folder making
-  string Fullpath = path + Bs + TeacherName + Bs + "Classes"  + Bs + ClassName; // Path to folder;
-  makefolder(StudentName, Fullpath, Warning);
-  string classname = StudentName;
-  string content = StudentName;
-  makefile(content, Fullpath, ClassName);
-
+  string Fullpath = path + Bs + TeacherName + Bs + "Classes" + Bs + ClassName; // Path to folder;
+  makefolder(Formatter(StudentName), Fullpath, Warning);
 }
-
+void interface()
+{
+  cout << "What do you want to do\nAddAClass\t[C]\nAddaStudent\t[S]\nAddateacher\t[T]\nExit\t\t[E]\n";
+  char input;
+  cin >> input;
+cout<< endl;
+  switch (toupper(input))
+  {
+    case 'C':
+    { AddAClass();
+    }
+    break;
+    case 'S':
+    { AddStudent();
+    }
+    break;
+    case 'T':
+    {  AddATeacher();
+    }
+    break;
+    case 'E':
+    {  system("exit");
+    }
+    break;
+    default:
+    {
+      cout <<"Invaild input try again"<<endl;
+    }
+    break;
+  }
+}
 int main()
-{   char Ob = 40;                // A Open bracket.
-    char Cb = 41;                // A Closed bracket.
-    char qm  = char(9);
-   string content;
-   content  = content +Ob+ "echo test  & echo. & echo line two"+Cb+">> C:\\logins\\Teachers\\file.txt";
-   system(Fixer(content));
-  system("tree /F  C:\\logins");
+{
+  
   startup();
-  // AddAClass();
-  AddStudent();
-  // AddATeacher();
-  system("tree /F  C:\\logins");
-  cout << "Hello World!\n";
+  while (true)
+  {
+    interface();
+  }
+  
 }
